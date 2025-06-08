cb0e6b7646aa85d6b4fd4663e24c2e9f6d4c1e4e
ragini <sy953126@gmail.com>
2025-06-08 20:22:33 +0530

    Fix : added backend as normal directory

---
+++ backend/src/webpack.config.js
@@ -0,0 +1,15 @@
+// const webpack = require('webpack');
+
+// module.exports = {
+//   resolve: {
+//     fallback: {
+//       path: require.resolve('path-browserify'),
+//       fs: false, // fs ब्राउज़र में काम नहीं करेगा
+//     },
+//   },
+//   plugins: [
+//     new webpack.ProvidePlugin({
+//       process: 'process/browser',
+//     }),
+//   ],
+// };
